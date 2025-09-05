399979d0e5047d41f5cb4d7b0002bbdecf242890
Alexander Stauch <blacke4dawn@gmail.com>
2025-09-05 14:17:26 +0200

    "fixed" page navigator Made sure it shows up to 11 pages, always first and last page, and ellipsis at second/second to last when needed.

--- src/components/sectors/pageNavigator.tsx
+++ src/components/sectors/pageNavigator.tsx
@@ -16,57 +16,46 @@ interface PageNavigatorProps {
 }
 
 export default function PageNavigator({ basePath, currentPage = 1, maxPages = 1 }: PageNavigatorProps) {
-  const showPreviousNext = maxPages > 7;
   const range = 3;
-  let startPage = Math.max(1, currentPage - range);
-  let endPage = Math.min(maxPages, currentPage + range);
-
-  if (endPage - startPage + 1 < 7) {
-    if (startPage === 1) {
-      endPage = Math.min(maxPages, startPage + range);
-    } else if (endPage === maxPages) {
-      startPage = Math.max(1, endPage - range);
-    }
-  }
-
-  const showFirstPage = startPage > 2;
-  const showLastPage = endPage < maxPages - 1;
-  const showStartEllipsis = startPage > 2;
-  const showEndEllipsis = endPage < maxPages - 1;
-
-  if (showFirstPage) startPage = Math.max(startPage, 4);
-
-  if (showLastPage) endPage = Math.min(endPage, maxPages - 3);
+  const startDynamicRange = Math.max(3, currentPage - range);
+  const endDynamicRange = Math.min(maxPages - 2, currentPage + range);
 
   return (
     <ContentContainer className="flex justify-center p-3">
       <Pagination>
         <PaginationContent>
-          {currentPage > 1 && showPreviousNext && (
+          {currentPage > 1 && (
             <PaginationItem>
               <PaginationPrevious href={`${basePath}?page=${currentPage - 1}`} />
             </PaginationItem>
           )}
 
-          {showFirstPage && (
+          <PaginationItem>
+            <PaginationLink
+              href={`${basePath}?page=1`}
+              isActive={currentPage === 1}
+            >
+              1
+            </PaginationLink>
+          </PaginationItem>
+
+          {currentPage > 6 ? (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          ) : (
             <PaginationItem>
               <PaginationLink
-                href={`${basePath}?page=1`}
-                isActive={currentPage === 1}
+                href={`${basePath}?page=2`}
+                isActive={currentPage === 2}
               >
-                1
+                2
               </PaginationLink>
             </PaginationItem>
           )}
 
-          {showStartEllipsis && (
-            <PaginationItem>
-              <PaginationEllipsis />
-            </PaginationItem>
-          )}
-
-          {[...Array(endPage - startPage + 1)].map((_, i) => {
-            const page = startPage + i;
+          {[...Array(endDynamicRange - startDynamicRange + 1)].map((_, i) => {
+            const page = startDynamicRange + i;
             return (
               <PaginationItem key={page}>
                 <PaginationLink
@@ -79,24 +68,30 @@ export default function PageNavigator({ basePath, currentPage = 1, maxPages = 1 
             );
           })}
 
-          {showEndEllipsis && (
+          {currentPage < maxPages - 5 ? (
             <PaginationItem>
               <PaginationEllipsis />
             </PaginationItem>
-          )}
-
-          {showLastPage && (
+          ) : (
             <PaginationItem>
               <PaginationLink
-                href={`${basePath}?page=${maxPages}`}
-                isActive={currentPage === maxPages}
+                href={`${basePath}?page=${maxPages - 1}`}
               >
-                {maxPages}
+                {`${maxPages - 1}`}
               </PaginationLink>
             </PaginationItem>
           )}
 
-          {currentPage < maxPages && showPreviousNext && (
+          <PaginationItem>
+            <PaginationLink
+              href={`${basePath}?page=${maxPages}`}
+              isActive={currentPage === maxPages}
+            >
+              {maxPages}
+            </PaginationLink>
+          </PaginationItem>
+
+          {currentPage < maxPages && (
             <PaginationItem>
               <PaginationNext href={`${basePath}?page=${currentPage + 1}`} />
             </PaginationItem>

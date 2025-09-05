a44dbfa736a72c0a9a0a8360c0fc7a4f6585a60c
Alexander Stauch <blacke4dawn@gmail.com>
2025-09-05 09:15:22 +0200

    types-tag highlight color

--- src/components/details/typeTag.tsx
+++ src/components/details/typeTag.tsx
@@ -4,7 +4,7 @@ export default function TypeTag({ name, highlighted }: { name: string; highlight
   const bgClr = getTypeColor(name);
   return (
     <span className={`px-2 py-1 rounded-full capitalize text-white text-center
-      ${ highlighted && "border-2 border-white"}
+      ${ highlighted && "border-2 border-black"}
       `} style={{ background: bgClr }}>
       {name}
     </span>
